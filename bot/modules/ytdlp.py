f5d6e7d7a92cce403692edd3427722a2fb295271
noneforhehe@gmail.com <noneforhehe@gmail.com>
2023-02-27 09:49:19 +0600

    Minor

--- bot/modules/ytdlp.py
+++ bot/modules/ytdlp.py
@@ -311,13 +311,13 @@ def ytdlZipleech(update, context):
     _ytdl(context.bot, update.message, True, True)
 
 ytdl_handler = CommandHandler(BotCommands.YtdlCommand, ytdl,
-                                filters=CustomFilters.authorized_user, run_async=True)
+                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 ytdl_zip_handler = CommandHandler(BotCommands.YtdlZipCommand, ytdlZip,
-                                    filters=CustomFilters.authorized_user, run_async=True)
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 ytdl_leech_handler = CommandHandler(BotCommands.YtdlLeechCommand, ytdlleech,
-                                filters=CustomFilters.authorized_user, run_async=True)
+                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 ytdl_zip_leech_handler = CommandHandler(BotCommands.YtdlZipLeechCommand, ytdlZipleech,
-                                    filters=CustomFilters.authorized_user, run_async=True)
+                                    filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 quality_handler = CallbackQueryHandler(select_format, pattern="qu", run_async=True)
 
 dispatcher.add_handler(ytdl_handler)
